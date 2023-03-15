2a386997e2b67465ad653583d80398f86b7a652a
Vasyl Lendo <vLendyo@devvision.org>
2023-03-15 14:24:11 +0200

    home work 08.03

---
+++ task_1/task_1/task_1.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+int main() {
+	int q = 1;
+	while (q != 0) {
+		//символ яким буде виводитись лінія
+		cout << "\nSet the symbol: ";
+		char a;
+		cin >> a;
+
+		//к-сть символів для лінії
+		cout << "Set length a line: ";
+		int b;
+		cin >> b;
+
+		//як саме буде виводитись лінія
+		cout << "Choose v - vertical or h - horizontal line: ";
+		char line;
+		cin >> line;
+
+
+		if (line == 'h'){
+			//швидкість з якою буде виводитись лінія
+			cout << "How do you want to print a line(s - slowly, n - normal, f - fast): ";
+			char speed;
+			cin >> speed;
+			if (speed == 's') {
+				for (int i = 0; i <= b; i++) {
+					cout << a;
+					Sleep(200);
+				}
+			}
+			else if (speed == 'n') {
+				for (int i = 0; i <= b; i++) {
+					cout << a;
+					Sleep(80);
+				}
+			}
+			else if (speed == 'f') {
+				for (int i = 0; i <= b; i++) {
+					cout << a;
+					Sleep(20);
+				}
+			}
+			else {
+				cout << "Invalid" << endl;
+			}
+		}
+		else if (line == 'v') {
+			//швидкість з якою буде виводитись лінія
+			cout << "How do you want to print a line(s - slowly, n - normal, f - fast): ";
+			char speed;
+			cin >> speed;
+			if (speed == 's') {
+				for (int i = 0; i <= b; i++) {
+					cout << a << endl;
+					Sleep(200);
+				}
+			}
+			else if (speed == 'n') {
+				for (int i = 0; i <= b; i++) {
+					cout << a << endl;
+					Sleep(80);
+				}
+			}
+			else if (speed == 'f') {
+				for (int i = 0; i <= b; i++) {
+					cout << a << endl;
+					Sleep(20);
+				}
+			}
+			else {
+				cout << "Invalid" << endl;
+			}
+		
+		}
+		
+		
+		else {
+			cout << "Invalid, try again";
+		}
+		cout << "\nDo you want to continue or exit(1 - yes/ 0 - no)" << endl;
+		cin >> q;
+	}		
+}
+		
---
+++ task_2/task_2/task_2.cpp
@@ -0,0 +1,267 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+
+int main() {
+	cout << "\t\tWelcome to the game \"Dices\"!" << endl;
+
+	//Перевіряємо, чи гравець хоче почати гру
+	char start;
+	cout << "Are you ready to start?(y/n): ";
+	cin >> start;
+
+	if (start == 'n') {
+		cout << "Ok, good bye!" << endl;
+		return 0;
+	}
+
+	char q = ' ';
+	while (q != 0) {
+		//генерація випадкових чисел
+		srand(time(NULL));
+
+		//змінні для загальних очків
+		int playerScore = 0, computerScore = 0;
+
+		//Три раунда
+		for (int i = 1; i <= 3; i++) {
+			//хід гравця
+			cout << "\tRAUND " << i << endl;
+			cout << "Player, roll the dices(press \"Enter\" to roll)..." << endl;
+			cin.get();
+
+			int playerRoll1 = rand() % 6 + 1;
+			Sleep(2000);
+			switch (playerRoll1) {
+			case 1:
+				cout << " ------- " << endl;
+				cout << "|       |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|       |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 2:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|       |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 3:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 4:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|       |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 5:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 6:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			}
+			int playerRoll2 = rand() % 6 + 1;
+			Sleep(1000);
+			switch (playerRoll2) {
+			case 1:
+				cout << " ------- " << endl;
+				cout << "|       |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|       |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 2:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|       |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 3:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 4:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|       |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 5:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 6:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			}
+			int playerRaundScore = playerRoll1 + playerRoll2;
+
+			Sleep(1000);
+			cout << "You rolled: " << playerRoll1 << " and " << playerRoll2 << endl << endl;
+
+			//хід компютера
+			cout << "The computer rolls the dices..." << endl;
+			int computerRoll1 = rand() % 6 + 1;
+			Sleep(2000);
+			switch (computerRoll1) {
+			case 1:
+				cout << " ------- " << endl;
+				cout << "|       |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|       |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 2:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|       |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 3:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 4:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|       |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 5:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 6:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			}
+			int computerRoll2 = rand() % 6 + 1;
+			Sleep(1000);
+			switch (computerRoll2) {
+			case 1:
+				cout << " ------- " << endl;
+				cout << "|       |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|       |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 2:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|       |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 3:
+				cout << " ------- " << endl;
+				cout << "| *     |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "|     * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 4:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|       |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 5:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "|   *   |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			case 6:
+				cout << " ------- " << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << "| *   * |" << endl;
+				cout << " ------- " << endl;
+				break;
+			}
+			int computerRaundScore = computerRoll1 + computerRoll2;
+
+			Sleep(1000);
+			cout << "The computer rolled: " << computerRoll1 << " and " << computerRoll2 << endl;
+			Sleep(1000);
+
+			//Обчислюємо результать раунду
+			if (playerRaundScore > computerRaundScore) {
+				playerScore++;
+				cout << "You win this raund!" << endl;
+			}
+			else if (playerRaundScore < computerRaundScore) {
+				computerScore++;
+				cout << "The computer won this raund!" << endl;
+			}
+			else {
+				cout << "Draw in this raund" << endl;
+			}
+
+			//Виводимо поточний рахунок
+			cout << "Current score: Player - " << playerScore << ", Computer - " << computerScore << endl << endl;
+		}
+
+
+		//Виводимо загальний результат
+		if (playerScore > computerScore) {
+			cout << "Congratulation!!! You won" << endl;
+		}
+		else if (playerScore < computerScore) {
+			cout << "The computer won" << endl;
+		}
+		else {
+			cout << "The game ended in a draw" << endl;
+		}
+
+		cout << "\nDo you want to try again?(y/n)";
+		cin >> q;
+
+		if (q == 'n') {
+			q = 0;
+		}
+	}
+}
